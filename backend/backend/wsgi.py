1e8a59c5d659059da11453820d4cddf3e246d2c8
Al Estilo Arias <alestiloarias@gmail.com>
2026-04-13 20:56:26 +0000

    Update wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+
 import os
 
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'backend.backend.settings'
+)
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
-
 application = get_wsgi_application()
+
